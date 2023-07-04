ce9c470a6826754457e06e6f5e3b682775054caf
WONHO22 <dnjsgh2122@gmail.com>
2023-07-05 02:07:15 +0900

    [Remove] 필요없는 주석 제거

--- src/App.js
+++ src/App.js
@@ -5,37 +5,6 @@ import Recomandation from "./pages/Recomandation";
 import "./App.css";
 import React, { useRef, useState, useEffect } from "react";
 
-// const BackgroundImage = styled.div`
-//   background-image: url("/images/unnamed.png");
-//   background-repeat: no-repeat;
-//   background-size: cover;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-// `;
-
-// const modalBackgroundImage = styled.div`
-//   background-image: url("/images/unnamed.png");
-//   background-repeat: no-repeat;
-//   background-size: cover;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   &::before {
-//     content: "";
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     width: 100%;
-//     height: 100%;
-//     background-color: rgba(0, 0, 0, 0.6); /* Adjust the opacity as desired */
-//   }
-// `;
-
 function App() {
   return (
     <BrowserRouter>
