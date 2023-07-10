3c7c217486ae79700a299fd8e443b3c3ba4b5973
yebin <yebind3308@gmail.com>
2023-07-10 22:35:11 +0900

    🐛 [Recomandation.js] 이미지 엑스박스 수정, url 줄 넘침 수정

--- src/pages/Recomandation.js
+++ src/pages/Recomandation.js
@@ -255,8 +255,7 @@ const InformationWrap = styled.div`
     margin: 10px;
     > a {
       width: 430px;
-      line-height: 1.8rem;
-      margin-left: 1rem;
+      padding-left: 1rem;
       overflow: hidden;
       text-overflow: ellipsis;
       &:link {
@@ -603,6 +602,7 @@ const Recomandation = () => {
                         image1
                           .filter((item) => item.title.includes("맛집"))
                           .filter((item) => !item.thumbnail.includes("output"))
+                          .filter((item) => !item.thumbnail.includes("cyworld"))
                           .slice(0, 5)
                           .map((item, index) => (
                             <div key={index}>
@@ -615,6 +615,7 @@ const Recomandation = () => {
                         image2
                           .filter((item) => item.title.includes("맛집"))
                           .filter((item) => !item.thumbnail.includes("output"))
+                          .filter((item) => !item.thumbnail.includes("cyworld"))
                           .slice(0, 5)
                           .map((item, index) => (
                             <div key={index}>
